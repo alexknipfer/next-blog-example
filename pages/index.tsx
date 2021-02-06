5711b0a72634904269d9bfb20e9657746ef4a4bb
alexknipfer <alexanderknipfer@gmail.com>
2021-02-06 00:48:07 -0500

    Move all posts query to gql file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,23 @@
 import BlogLayout from '@/layouts/BlogLayout';
 import { addApolloState, initializeApollo } from '@/lib/apolloClient';
 import { Post } from '@/models/Post';
-import { gql } from '@apollo/client';
-import Image from 'next/image';
 import {
   Box,
-  Button,
   Heading,
   Text,
   Link as ChakraLink,
-  HStack,
   Divider,
 } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import Link from 'next/link';
-
-const ALL_POSTS_QUERY = gql`
-  query AllPostsQuery {
-    posts {
-      id
-      slug
-      title
-      date
-      author {
-        name
-        title
-        picture {
-          url
-        }
-      }
-    }
-  }
-`;
+import Author from '@/components/Author';
+import ALL_POSTS_QUERY from '@/graphql/allPostsQuery.graphql';
 
 interface Props {
   posts: Post[];
 }
 
 const BlogPosts: NextPage<Props> = ({ posts }) => {
-  console.log('POSTS: ', posts);
   return (
     <BlogLayout>
       <Heading as="h1" mb={10}>
@@ -52,18 +31,11 @@ const BlogPosts: NextPage<Props> = ({ posts }) => {
               {post.title}
             </ChakraLink>
           </Text>
-          <HStack my={3}>
-            <Image
-              width={50}
-              height={50}
-              src={post.author.picture.url}
-              className="profile-image"
-            />
-            <Box>
-              <Text color="gray.300">{post.author.name}</Text>
-              <Text color="gray.400">{post.author.title}</Text>
-            </Box>
-          </HStack>
+          <Author
+            profilePicture={post.author.picture.url}
+            name={post.author.name}
+            title={post.author.title}
+          />
           <Divider my={5} />
         </Box>
       ))}
