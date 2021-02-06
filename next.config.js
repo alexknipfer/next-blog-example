505c7501fb17efac1661fe2ac4de88d296a1e3d0
alexknipfer <alexanderknipfer@gmail.com>
2021-02-06 00:46:01 -0500

    Add support for graphql files

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@ module.exports = {
   images: {
     domains: ['media.graphcms.com'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+
+    return config;
+  },
+  webpackDevMiddleware: (config) => {
+    return config;
+  },
 };
